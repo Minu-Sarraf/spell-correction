8e85ef918a7ed9e8ce148d31a0fc0b5f8cdd70d9
minu <minusarraf96@gmail.com>
2019-03-05 10:01:22 +0545

    Spell-correction

---
+++ spellcorrection/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to spellcorrection
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ spellcorrection/test/spellcorrection/core_test.clj
@@ -0,0 +1,7 @@
+(ns spellcorrection.core-test
+  (:require [clojure.test :refer :all]
+            [spellcorrection.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
